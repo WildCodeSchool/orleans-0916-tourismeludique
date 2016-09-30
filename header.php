67da8f2ed124447a03175206d6b9983de6542f89
MathieuM1 <mat.mercier45@gmail.com>
2016-09-30 10:25:04 +0200

    ajout header.php

---
+++ header.php
@@ -0,0 +1,8 @@
+<div class="jumbotron">
+	<div class="container-fluid">
+		<div class="col-lg-12 col-md-12">
+			<img class="img-responsive logo" src="Logo_mTourismeLudique_red(1).jpg" alt="Logo mTourimse-Ludique">
+		</div>
+	</div>
+</div>
+
