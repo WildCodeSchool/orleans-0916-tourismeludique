66092466cc6ca89a3e53f102c1a116b1de410e7b
louisemunier <munier.louise0@gmail.com>
2016-09-30 14:04:13 +0200

    Added stylesheet_main1.css ans added bootstrap to partie 2

--- main1.php
+++ main1.php
@@ -1,14 +1,14 @@
 <main>
     <div class="container-fluid">
 
-        <!----- Partie 1 : Notre philosophie ----->
+<!----- Partie 1 : Notre philosophie ----->
         <div class="row">
             <!----- Espace vide à gauche ----->
-            <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1" style="background-color: lightblue";></div>
+            <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1"></div>
 
             <!----- Image ----->
             <div class="col-xs-6 col-sm-6 col-md-5 col-lg-5">
-                <img src="image/lens_reflect.png" alt="image dans lentille photo" width="50%" height="50%">
+                <img class="img-responsive" src="image/lens_reflect.png" alt="image dans lentille photo" style="width:45%;height:45%">
             </div>
 
             <!----- titre + text ----->
@@ -18,8 +18,42 @@
             </div>
 
             <!----- Espace vide à droite ----->
-            <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1" style="background-color: blueviolet";></div>
-            
+            <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1"></div>
+
+        </div>
+
+        <hr/>
+
+
+<!----- Partie 2 : bandeau 3 pts ----->
+        <div class="row">
+            <!----- Espace vide à gauche ----->
+            <div class="col-xs-0 col-sm-1 col-md-2 col-lg-3"></div>
+
+            <!----- point 1 ----->
+            <div class="col-xs-12 col-sm-2 col-md-2 col-lg-2 point123">
+                <div class="col-xs-5 col-xs-offset-3 col-sm-2 col-md-2 col-lg-3"><h3>Susciter l'intérêt</h3></div>
+                <div class="col-xs-4 col-sm-2 col-md-2 col-lg-3"><img class="img-responsive" src="image/atom.png" alt="" width="100px" height="100px"></div>
+                <div class="hidden-xs col-sm-1 col-md-2 col-lg-3"><p>Accedat huc suavitas quaedam oportet sermonum atque morum, haudquaquam mediocre condimentum amicitiae. Tristitia autem et in omni re severitas habet illa quidem gravitatem.</p></div>
+            </div>
+
+            <!----- point 2 ----->
+            <div class="col-xs-12 col-sm-2 col-md-2 col-lg-2 point123">
+                <div class="col-xs-5 col-xs-offset-3 col-sm-2 col-md-2 col-lg-3"><h3>Etablir un lien</h3></div>
+                <div class="col-xs-4 col-sm-2 col-md-2 col-lg-3"><img class="img-responsive" src="image/atom.png" alt="" width="100px" height="100px"></div>
+                <div class="hidden-xs col-sm-1 col-md-2 col-lg-3"><p>Accedat huc suavitas quaedam oportet sermonum atque morum, haudquaquam mediocre condimentum amicitiae. Tristitia autem et in omni re severitas habet illa quidem gravitatem.</p></div>
+            </div>
+
+            <!----- point 3 ----->
+            <div class="col-xs-12 col-sm-6 col-md-2 col-lg-2 point123">
+                <div class="col-xs-5 col-xs-offset-3 col-sm-2 col-md-2 col-lg-3"><h3>Donner du sens</h3></div>
+                <div class="col-xs-4 col-sm-2 col-md-2 col-lg-3"><img class="img-responsive" src="image/atom.png" alt="" width="100px" height="100px"></div>
+                <div class="hidden-xs col-sm-1 col-md-2 col-lg-3"><p>Accedat huc suavitas quaedam oportet sermonum atque morum, haudquaquam mediocre condimentum amicitiae. Tristitia autem et in omni re severitas habet illa quidem gravitatem.</p></div>
+            </div>
+
+            <!----- Espace vide à droite ----->
+            <div class="col-xs-0 col-sm-1 col-md-2 col-lg-3"></div>
+
         </div>
 
 
