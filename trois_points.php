792a7b3223cc41bfc384b5136bab5ebff45160f5
louisemunier <munier.louise0@gmail.com>
2016-10-03 12:12:56 +0200

    Boostrap trois_points

--- trois_points.php
+++ trois_points.php
@@ -1,31 +1,31 @@
 <!----- Partie 2 : bandeau 3 pts ----->
 <div class="row">
     <!----- Espace vide à gauche ----->
-    <div class="col-xs-0 col-sm-1 col-md-1 col-lg-1"></div>
+    <!--- <div class="col-xs-0 col-sm-1 col-md-1 col-lg-1"></div> --->
 
     <!----- point 1 ----->
-    <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3 point123">
-        <div class="col-xs-5 col-sm-2 col-md-10 col-lg-10 titre_trois_points"><h3>Susciter l'intérêt</h3></div>
-        <div class="col-xs-4 col-sm-2 col-md-10 col-lg-10"><img class="img-responsive img_trois_points" src="image/atom.png" alt="" width="100px" height="100px"></div>
-        <div class="hidden-xs col-sm-1 hidden-sm col-md-10 col-lg-10 text_trois_points"><p>Accedat huc suavitas quaedam oportet sermonum atque morum, haudquaquam mediocre condimentum amicitiae. Tristitia autem et in omni re severitas habet illa quidem gravitatem.</p></div>
+    <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 point123">
+        <div class="col-xs-6 col-sm-6 col-md-7 col-md-offset-5 col-lg-7 col-lg-offset-5 titre_trois_points"><h3>Susciter l'intérêt</h3></div>
+        <div class="col-xs-6 col-sm-6 col-md-7 col-md-offset-5 col-lg-7 col-lg-offset-5"><img class="img-responsive img_trois_points" src="image/atom.png" alt="" width="150px" height="200px"></div>
+        <div class="hidden-xs col-sm-7 col-sm-offset-5 col-md-7 col-md-offset-5 col-lg-7 col-lg-offset-5 text_trois_points"><p>Accedat huc suavitas quaedam oportet sermonum atque morum, haudquaquam mediocre condimentum amicitiae. Tristitia autem et in omni re severitas habet illa quidem gravitatem.</p></div>
     </div>
 
     <!----- point 2 ----->
-    <div class="col-xs-12 col-sm-2 col-md-2 col-lg-2 point123">
-        <div class="col-xs-5 col-sm-2 col-md-12 col-lg-12 titre_trois_points"><h3>Etablir un lien</h3></div>
-        <div class="col-xs-4 col-sm-2 col-md-12 col-lg-12"><img class="img-responsive img_trois_points" src="image/atom.png" alt="" width="100px" height="100px"></div>
-        <div class="hidden-xs col-sm- hidden-sm col-md-2 col-lg-12 text_trois_points"><p>Accedat huc suavitas quaedam oportet sermonum atque morum, haudquaquam mediocre condimentum amicitiae. Tristitia autem et in omni re severitas habet illa quidem gravitatem.</p></div>
+    <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 point123">
+        <div class="col-xs-6 col col-xs-push- col-sm-6 col-md-7 col-md-offset-3 col-lg-7 col-lg-offset-3 titre_trois_points"><h3>Etablir un lien</h3></div>
+        <div class="col-xs-6 col-xs-pull- col-sm-6 col-md-7 col-md-offset-3 col-lg-7 col-lg-offset-3"><img class="img-responsive img_trois_points" src="image/atom.png" alt="" width="150px" height="200px"></div>
+        <div class="hidden-xs col-sm-7 col-sm-offset-5 col-md-7 col-md-offset-3 col-lg-7 col-lg-offset-3 text_trois_points"><p>Accedat huc suavitas quaedam oportet sermonum atque morum, haudquaquam mediocre condimentum amicitiae. Tristitia autem et in omni re severitas habet illa quidem gravitatem.</p></div>
     </div>
 
     <!----- point 3 ----->
-    <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3 point123">
-        <div class="col-xs-5 col-sm-2 col-md-10 col-lg-10 titre_trois_points"><h3>Donner du sens</h3></div>
-        <div class="col-xs-4 col-sm-2 col-md-10 col-lg-10"><img class="img-responsive img_trois_points" src="image/atom.png" alt="" width="100px" height="100px"></div>
-        <div class="hidden-xs col-sm-1 hidden-sm col-md-10 col-lg-10 text_trois_points"><p>Accedat huc suavitas quaedam oportet sermonum atque morum, haudquaquam mediocre condimentum amicitiae. Tristitia autem et in omni re severitas habet illa quidem gravitatem.</p></div>
+    <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 point123">
+        <div class="col-xs-6 col-sm-6 col-md-7 col-lg-7 titre_trois_points"><h3>Donner du sens</h3></div>
+        <div class="col-xs-6 col-sm-6 col-md-7 col-lg-7"><img class="img-responsive img_trois_points" src="image/atom.png" alt="" width="150px" height="200px"></div>
+        <div class="hidden-xs col-sm-7 col-md-7 col-lg-7 text_trois_points"><p>Accedat huc suavitas quaedam oportet sermonum atque morum, haudquaquam mediocre condimentum amicitiae. Tristitia autem et in omni re severitas habet illa quidem gravitatem.</p></div>
     </div>
 
     <!----- Espace vide à droite ----->
-    <div class="col-xs-0 col-sm-1 col-md-1 col-lg-1"></div>
+    <!--- <div class="col-xs-0 col-sm-1 col-md-1 col-lg-1"></div> --->
 
 </div>
 
