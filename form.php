3480e583190d4450c72bcd9d0017412cd86d38ab
MathieuM1 <mat.mercier45@gmail.com>
2016-10-04 19:59:35 +0200

    modif font-size form

--- form.php
+++ form.php
@@ -1,9 +1,9 @@
 <div class="sectionForm" id="ancreForm">
     <div class="row">
         <div class="col-lg-6 col-md-6 col-sm-6 hidden-xs leftSide">
-            <p class="accrocheForm">
+            <h2 class="accrocheForm">
                 Vous souhaitez mettre en valeur votre patrimoine architecural, promouvoir votre commune...
-            </p>
+            </h2>
             <img src="image/P1030919rond.png" class="img-responsive imgInLens" alt="gargouille">
             <img src="image/lens_reflect.png"  class="img-responsive lens" alt="lentille">
         </div>
@@ -12,27 +12,27 @@
             <h2 class="titreForm">Une idée? Un projet?<br/>Contactez-nous</h2>
             <form method="post" action="formPost.php">
                 <div class="form-group">
-                    <label for="Nom">Nom *</label>
+                    <label for="Nom"><p>Nom *</p></label>
                     <input type="Nom" class="form-control" id="Nom" placeholder="Votre nom..." required>
                 </div>
                 <div class="form-group">
-                    <label for="Prenom">Prénom *</label>
+                    <label for="Prenom"><p>Prénom *</p></label>
                     <input type="Prenom" class="form-control" id="Prenom" placeholder="Votre prénom..."required>
                 </div>
                 <div class="form-group">
-                    <label for="Entreprise">Entreprise, société, organisme...</label>
+                    <label for="Entreprise"><p>Entreprise, société, organisme...</p></label>
                     <input type="Entreprise" class="form-control" id="Entreprise" placeholder="Votre entreprise, société organisme..."required>
                 </div>
                 <div class="form-group">
-                    <label for="Email">Mail *</label>
+                    <label for="Email"><p>Mail *</p></label>
                     <input type="email" class="form-control" id="Email" placeholder="Votre Email..."required>
                 </div>
                 <div class="form-group">
-                    <label for="Message">Message *</label>
+                    <label for="Message"><p>Message *</p></label>
                     <textarea name="Message" class="form-control" placeholder="Votre message..." required></textarea>
                 </div>
                 <div class="form-group">
-                    <button type="submit" class="btn btn-primary">Envoyer</button>
+                    <button type="submit" class="btn btn-primary"><p>Envoyer</p></button>
                 </div>
             </form>
         </div>
