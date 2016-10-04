2b7c9389a20cf9e9273d57477603ab235fdab842
Gy0m <landreaug@gmail.com>
2016-10-04 17:26:19 +0200

    ajout image rond, bidouillage produits

--- produits.php
+++ produits.php
@@ -1,17 +1,28 @@
 <div class=" container-fluid produits" id="ancreProduit">
+    <div class="row">
     <div class="produitsText col-lg-6">
         <h1>Nos Produits</h1>
         <h2>Géomotifs</h2>
-        <p>Quae et ut iusto qui. Unde et quia reiciendis iste dolore est enim odio. Ullam nihil molestiae mollitia 
+        <p>Quae et ut iusto qui. Unde et quia reiciendis iste dolore est enim odio. Ullam nihil molestiae mollitia
             architecto ea. Accusantium est quis est nesciunt eaque. Culpa aut ratione sit.<br/>
-            Officia expedita eligendi doloribus quia officiis. Illo voluptatem optio quidem eligendi. 
-            Quis aut quod consequatur autem beatae illo. Iste saepe tempora voluptas minus. Debitis perferendis error 
+            Officia expedita eligendi doloribus quia officiis. Illo voluptatem optio quidem eligendi.
+            Quis aut quod consequatur autem beatae illo. Iste saepe tempora voluptas minus. Debitis perferendis error
             ullam adipisci ea distinctio sit in.
         </p>
-        <a href="https://itunes.apple.com/fr/app/geomotifs/id935219020" target="_blank"><img src="image/appStore.svg" class="img-responsive logo" alt="Telecharger dans l'app store"></a>
-        <a href="https://play.google.com/store/apps/details?id=com.mazedia.wezit.geomotifs" target="_blank"><img src="image/googlePlay.png" class="img-responsive logo" alt="Telecharger dans Google Play"></a>
+            <a href="https://itunes.apple.com/fr/app/geomotifs/id935219020" target="_blank"><img src="image/appStore.svg" class="img-responsive logo" alt="Telecharger dans l'app store"></a>
+            <a href="https://play.google.com/store/apps/details?id=com.mazedia.wezit.geomotifs" target="_blank"><img src="image/googlePlay.png" class="img-responsive logo" alt="Telecharger dans Google Play"></a>
     </div>
+    </div>
+    <div class"row">
+    <div class="col-lg-3 col-lg-offset-8 col-md-6">
+        <div class="embed-responsive embed-responsive-4by3">
+            <iframe class="video" width="250" height="180" src="https://www.youtube.com/embed/Svd4SGOJTfI" allowfullscreen></iframe>
+        </div>
+    </div>
+    </div>
+    <!--- <div class="row">
     <div class="produitsImg col-lg-6">
         <img src="image/lens_reflect.png" class="img-responsive lensRight" alt="image produit">
     </div>
+</div> --->
 </div>
