2320befe1ec03e3daba8410d62d0c1c769af18fa
louisemunier <munier.louise0@gmail.com>
2016-10-05 17:32:27 +0200

    Added box FB

--- mTourisme.php
+++ mTourisme.php
@@ -39,6 +39,17 @@
   </head>
 
 <body>
+
+<script>(function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v2.7";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+</script>
+
+
 	<!-- Header -->
     <?php include ('header.php'); ?>
     <?php include ('social_network.php'); ?>
--- social_network.php
+++ social_network.php
@@ -6,14 +6,36 @@
             <a href="https://www.facebook.com/mtourismeludique">
                 <img alt="Facebook MTourismeLudique" src="image/logo_FB.png"/>
             </a>
-            <!--- <div id="infos_FB" class="bloc_infos_FB">
+
+            <div class="fb-page" data-href="https://www.facebook.com/mtourismeludique"
+                 data-tabs="timeline"
+                 data-small-header="true"
+                 data-adapt-container-width="true"
+                 data-hide-cover="true"
+                 data-show-facepile="false">
+
+                <div class="fb-xfbml-parse-ignore">
+                    <a href="https://www.facebook.com/mtourismeludique"></a>
+                </div>
+                <div id="fb-root">
+                    <iframe class="box_FB" src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fmtourismeludique&tabs=timeline&width=340&height=500&small_header=true&adapt_container_width=true&hide_cover=true&show_facepile=false&appId"
+                            width="340"
+                            height="500">
+                            <!-- style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"-->
+                    </iframe>
+                </div>
+            </div>
+            <!---
+            <div id="infos_FB" class="bloc_infos_FB">
+
                 <div class="nom_bloc_infos">Facebook</div>
                 <div class="text_bloc_infos">
                     <div id="FB-root" class="FB-reset">
-
+                        <iframe id="FB_frame-http" name="FB_frame_http" allowtransparency="true" allowfullscreen="true" scrolling="no" aria-hidden="true" title="Facebook Infos" tabindex="-1" style="border: none;" src=" " frameborder="0"></iframe>
                     </div>
                 </div>
-            </div> --->
+            </div>
+            --->
         </li>
 
         <!--- Bouton YouTube --->
