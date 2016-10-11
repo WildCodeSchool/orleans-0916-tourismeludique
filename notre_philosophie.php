c46aa5258c74c0b28363442b91ddcd976a7f9c49
MathieuM1 <mat.mercier45@gmail.com>
2016-10-11 15:59:38 +0200

    amelioration code notre philo

--- notre_philosophie.php
+++ notre_philosophie.php
@@ -1,4 +1,4 @@
-<main>
+<main xmlns="http://www.w3.org/1999/html">
     <div class="container-fluid" id="ancreMetier">
 
 <!----- Partie 1 : Notre philosophie ----->
@@ -7,12 +7,12 @@
             <!----- titre + text ----->
             <div class="col-xs-6 col-sm-6 col-md-5 col-lg-12">
                     <h1 class="titreMetier">Notre métier</h1>
-                    <p class="txt-presentation">Créée fin 2013, <strong>m-Tourisme Ludique</strong> est une société d’édition d’applications mobiles, installée dans le Loiret.<br></p>
+                    <p class="txt-presentation">Créée fin 2013, <span class="underline">m-Tourisme Ludique</span> est une société d’édition d’applications mobiles, installée dans le Loiret.<br></p>
 
                 <!--- liste sans puces --->
                 <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                     <div class="hidden-xs hidden-sm col-md-3 col-lg-3"></div>
-                    <ul class="col-xs-12 col-sm-12 col-md-6 col-lg-6 liste_philo" style="list-style-type:none">
+                    <ul class="col-xs-12 col-sm-12 col-md-6 col-lg-6 liste_philo">
                         <li><strong>Notre vocation :</strong>
                             Valoriser votre patrimoine matériels ou immatériels.
                         </li>
