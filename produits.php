5f832f51620b39183010bff0f2889eae354ee29d
Gy0m <landreaug@gmail.com>
2016-10-11 16:54:17 +0200

    add quotes img and iphone img

--- produits.php
+++ produits.php
@@ -1,22 +1,27 @@
 <div class=" container-fluid produits" id="ancreProduit">
     <div class="row">
-    <div class="produitsText col-xs-6">
-        <h1>Nos Produits</h1>
-        <h2>Géomotifs</h2>
-        <p>Quae et ut iusto qui. Unde et quia reiciendis iste dolore est enim odio. Ullam nihil molestiae mollitia
-            architecto ea. Accusantium est quis est nesciunt eaque. Culpa aut ratione sit.<br/>
-            Officia expedita eligendi doloribus quia officiis. Illo voluptatem optio quidem eligendi.
-            Quis aut quod consequatur autem beatae illo. Iste saepe tempora voluptas minus. Debitis perferendis error
-            ullam adipisci ea distinctio sit in.
-        </p>
-
-
-            <a href="https://itunes.apple.com/fr/app/geomotifs/id935219020" target="_blank"><img src="image/appStore.svg" class="img-responsive itunes" alt="Telecharger dans l'app store"></a>
-            <a href="https://play.google.com/store/apps/details?id=com.mazedia.wezit.geomotifs" target="_blank"><img src="image/googlePlay.png" class="img-responsive android" alt="Telecharger dans Google Play"></a>
-    </div>
-    <div class="col-xs-4 col-md-offset-1">
+        <div class="produitsText col-xs-7 col-lg-7">
+            <h1>Nos Produits</h1>
+            <h2>Géomotifs</h2>
+            <p>Quae et ut iusto qui. Unde et quia reiciendis iste dolore est enim odio. Ullam nihil molestiae mollitia
+                architecto ea. Accusantium est quis est nesciunt eaque. Culpa aut ratione sit.<br/>
+                Officia expedita eligendi doloribus quia officiis. Illo voluptatem optio quidem eligendi.
+                Quis aut quod consequatur autem beatae illo. Iste saepe tempora voluptas minus. Debitis perferendis error
+                ullam adipisci ea distinctio sit in.
+            </p>
+            <a href="https://itunes.apple.com/fr/app/geomotifs/id935219020" target="_blank"><img
+                    src="image/logoAppStore.png" class="img-responsive itunes" alt="Telecharger dans l'app
+                    store"></a>
+            <a href="https://play.google.com/store/apps/details?id=com.mazedia.wezit.geomotifs" target="_blank"><img src="image/logoGooglePlay.png" class="img-responsive android" alt="Telecharger dans Google Play"></a>
+        </div>
+    <div class="col-xs-4 col-lg-4">
         <div class="embed-responsive embed-responsive-4by3">
-            <iframe class="video" width="650" height="880" src="https://www.youtube.com/embed/Svd4SGOJTfI" allowfullscreen></iframe>
+            <iframe class="video" width="650" height="880" src="https://www.youtube.com/embed/Svd4SGOJTfI"
+                    allowfullscreen></iframe>
         </div>
     </div>
+    </div>
+
+
 </div>
+
--- sectionImage.php
+++ sectionImage.php
@@ -1,5 +1,16 @@
 <html>
-<div class="hidden-xs sectionImage">
-	<p class="textImage">"Redecouvrez ce que vous voyez tous les jours"</p>
+<div class="container-fluid">
+    <div class="row">
+    <div class="hidden-xs sectionImage">
+        <div class="col-xs-4">
+	        <img class="guillemets" src="image/guillemets up.png" alt="image guillemets">
+        </div>
+	        <p class="textImage">Redecouvrez ce que vous voyez tous les jours</p>
+        <div class="col-xs-4">
+            <img class="guillemetsdown" src="image/guillemets down.png" alt="image guillemets">
+        </div>
+    </div>
+    </div>
 </div>
+
 </html>
