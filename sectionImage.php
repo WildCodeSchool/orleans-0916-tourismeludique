4a4d315806ab1f0cc1373c350904a8993d5f532f
Gy0m <landreaug@gmail.com>
2016-10-11 17:51:52 +0200

    minor changes

--- sectionImage.php
+++ sectionImage.php
@@ -2,15 +2,16 @@
 <div class="container-fluid">
     <div class="row">
     <div class="hidden-xs sectionImage">
-        <div class="col-xs-4">
+        <div class="col-xs-offset-2 col-xs-8">
+            <br/><br/>
 	        <img class="guillemets" src="image/guillemets up.png" alt="image guillemets">
+	        <p class="textImage">Redécouvrez ce que vous voyez tous les jours</p>
         </div>
-	        <p class="textImage">Redecouvrez ce que vous voyez tous les jours</p>
-        <div class="col-xs-4">
+        <div class="col-xs-offset-10 col-xs-2">
+            <br/><br/><br/><br/>
             <img class="guillemetsdown" src="image/guillemets down.png" alt="image guillemets">
         </div>
     </div>
     </div>
 </div>
-
 </html>
