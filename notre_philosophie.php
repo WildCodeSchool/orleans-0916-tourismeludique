dbc14ef3bb16a94423bfb3609d4a9dffe1e21616
MathieuM1 <mat.mercier45@gmail.com>
2016-10-11 16:14:41 +0200

    refactorisation code notre philo

--- notre_philosophie.php
+++ notre_philosophie.php
@@ -1,40 +1,24 @@
 <main xmlns="http://www.w3.org/1999/html">
-    <div class="container-fluid" id="ancreMetier">
-
-<!----- Partie 1 : Notre philosophie ----->
-        <div class="row notre_philo">
-
-            <!----- titre + text ----->
-            <div class="col-xs-6 col-sm-6 col-md-5 col-lg-12">
-                    <h1 class="titreMetier">Notre métier</h1>
-                    <p class="txt-presentation">Créée fin 2013, <span class="underline">m-Tourisme Ludique</span> est une société d’édition d’applications mobiles, installée dans le Loiret.<br></p>
-
-                <!--- liste sans puces --->
-                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                    <div class="hidden-xs hidden-sm col-md-3 col-lg-3"></div>
-                    <ul class="col-xs-12 col-sm-12 col-md-6 col-lg-6 liste_philo">
-                        <li><strong>Notre vocation :</strong>
-                            Valoriser votre patrimoine matériels ou immatériels.
-                        </li>
-                        <li><strong>Nos atouts :</strong>
-                            Proposer des outils originaux, ludiques et pédagogiques.
-                        </li>
-                        <li><strong>Nos valeurs :</strong>
-                            Plaisir, curiosité, qualité, partage.
-                        </li>
-                    </ul>
-                    <div class="hidden-xs hidden-sm col-md-3 col-lg-3"></div>
-                </div>
-
-
-            </div>
-
+    <div class="container-fluid notre_philo" id="ancreMetier">
+        <!----- titre + text ----->
+        <div class="row">
+            <h1 class="titreMetier">Notre métier</h1>
+            <p class="txt-presentation">Créée fin 2013, <span class="underline">m-Tourisme Ludique</span> est une société d’édition d’applications mobiles, installée dans le Loiret.<br></p>
         </div>
-
-
-
-
-
+        <!--- liste sans puces --->
+        <div class="row">
+            <ul class="liste_philo">
+                <li><strong>Notre vocation :</strong>
+                    Valoriser votre patrimoine matériels ou immatériels.
+                </li>
+                <li><strong>Nos atouts :</strong>
+                    Proposer des outils originaux, ludiques et pédagogiques.
+                </li>
+                <li><strong>Nos valeurs :</strong>
+                    Plaisir, curiosité, qualité, partage.
+                </li>
+            </ul>
+        </div>
     </div>
 </main>
 
