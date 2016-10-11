1e18a2a0bacbd6168c71eff519e9c54b5c75ab76
MathieuM1 <mat.mercier45@gmail.com>
2016-10-11 15:48:35 +0200

    debug navbar

--- header.php
+++ header.php
@@ -16,9 +16,9 @@
     $(window).scroll(function (event) {
         // A chaque fois que l'utilisateur va scroller
         var y = $(this).scrollTop(); // On récupérer la valeur du scroll vertical
-
-        //si cette valeur > à 300 on ajouter la class
-        if (y >= 300)) {
+        var hauteur = document.getElementById('ancreHome').scrollHeight; // On récupère la hauteur du header
+        //si cette valeur > à hauteur on ajoute la class
+        if (y >= hauteur) {
             $('.navbar').addClass('fixed');
         } else {
             // sinon, on l'enlève
