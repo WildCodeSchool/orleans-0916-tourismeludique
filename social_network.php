d8adbb710b68dd9768e2b92248c9ce89d3f8251a
MathieuM1 <mat.mercier45@gmail.com>
2016-10-12 09:16:44 +0200

    modif social network

--- social_network.php
+++ social_network.php
@@ -13,7 +13,7 @@
 
         <!--- Bouton Facebook MLT --->
         <li class="bouton_facebook_MLT">
-            <a href="https://www.facebook.com/mtourismeludique">
+            <a href="https://www.facebook.com/mtourismeludique" target="_blank">
                 <img alt="Facebook MTourismeLudique" src="image/logo_FB.png"/>
             </a>
 
@@ -25,7 +25,7 @@
                  data-show-facepile="false">
 
                 <div class="fb-xfbml-parse-ignore">
-                    <a href="https://www.facebook.com/mtourismeludique" target="_blank"></a>
+                    <a href="https://www.facebook.com/mtourismeludique"></a>
                 </div>
                 <div id="fb-root">
                     <iframe class="box_FB" src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fmtourismeludique&tabs=timeline&width=340&height=500&small_header=true&adapt_container_width=true&hide_cover=true&show_facepile=false&appId"
@@ -40,7 +40,7 @@
 
         <!--- Bouton YouTube --->
         <li class="bouton_youtube">
-            <a href="https://www.youtube.com/channel/UCj_DmGMNzw3DTMUMyYoyoyQ">
+            <a href="https://www.youtube.com/channel/UCj_DmGMNzw3DTMUMyYoyoyQ" target="_blank">
                 <img alt="Youtube mTourismeLudique" src="image/logo_YT.png"/>
             </a>
 
@@ -66,7 +66,7 @@
 
         <!--- Bouton Likedin --->
         <li class="bouton_IN">
-            <a href="https://www.linkedin.com/in/sylvain-lambert-38387757">
+            <a href="https://www.linkedin.com/in/sylvain-lambert-38387757" target="_blank">
                 <img alt="Profil Sylvain Lambert" src="image/logo_linkedin.png"/>
             </a>
 
