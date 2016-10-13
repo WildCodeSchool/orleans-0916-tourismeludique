59793f3dbe87d4e5ebbd9148b85fb4a328d26dd7
MathieuM1 <mat.mercier45@gmail.com>
2016-10-13 19:07:51 +0200

    ajout carte section form, modif a venir dessus

--- form.php
+++ form.php
@@ -1,36 +1,44 @@
 <div class="sectionForm" id="ancreForm">
     <div class="row">
         <div class="col-lg-6 col-md-6 col-sm-6 hidden-xs leftSide">
-            <h2 class="accrocheForm">
-                Vous souhaitez mettre en valeur votre patrimoine architecural, promouvoir votre commune...
-            </h2>
-            <img src="image/P1030919rond.png" class="img-responsive imgInLensForm" alt="gargouille">
-            <img src="image/lens_reflect.png"  class="img-responsive lens" alt="lentille">
+            <div class="row">
+                <div class="col-lg-6">
+                    <img src="image/logolabodetoure.png" class="img-responsive logolabo" alt="logo Le Lab'O">
+                </div>
+                <div class="col-lg-6">
+                    <p class="adresse">
+                        Le Lab'O<br/>
+                        1 Avenue du champs de Mars<br/>
+                        Orléans
+                    </p>
+                </div>
+            </div>
+            <iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://www.openstreetmap.org/export/embed.html?bbox=1.8892514705657961%2C47.89133419118289%2C1.8968582153320315%2C47.8957799548847&amp;layer=mapnik&amp;marker=47.893557120745015%2C1.8930602073669434" style="border: 1px solid black"></iframe><br/>
+            <small><a  class="legendecarte" href="http://www.openstreetmap.org/?mlat=47.89356&amp;mlon=1.89306#map=17/47.89356/1.89305">View Larger Map</a></small>
         </div>
         <div class="col-lg-6 col-md-6 col-sm-6">
-            <h2 class="titreForm">Une idée? Un projet?<br/>Contactez-nous</h2><br/>
             <form method="post" action="formPost.php">
                 <div class="form-group">
-                    <input type="Nom" class="form-control input-lg" id="Nom" placeholder="Votre nom" aria-label="Votre nom"
+                    <input type="Nom" class="form-control input-lg" id="Nom" placeholder="Nom *" aria-label="Votre nom"
                            required><br/>
                 </div>
                 <div class="form-group">
-                    <input type="Prenom" class="form-control input-lg" id="Prenom" placeholder="Votre prénom"
+                    <input type="Prenom" class="form-control input-lg" id="Prenom" placeholder="Prénom"
                            aria-label="Votre prénom" required><br/>
                 </div>
                 <div class="form-group">
-                    <input type="Entreprise" class="form-control input-lg" id="Entreprise" placeholder="Votre entreprise, société organisme" aria-label="Votre entreprise, société, organisme"><br/>
+                    <input type="Entreprise" class="form-control input-lg" id="Entreprise" placeholder="Entreprise, Société, Organisme..." aria-label="Votre entreprise, société, organisme"><br/>
                 </div>
                 <div class="form-group">
-                    <input type="email" class="form-control input-lg" id="Email" placeholder="Votre Email..."
+                    <input type="email" class="form-control input-lg" id="Email" placeholder="Email*"
                            aria-label="Votre email" required><br/>
                 </div>
                 <div class="form-group">
-                    <textarea name="Message" class="form-control input-lg" placeholder="Votre message..." aria-label="Votre
+                    <textarea name="Message" class="form-control input-lg" placeholder="Message*" aria-label="Votre
                     message" rows="10" required></textarea><br/>
                 </div>
                 <div class="form-group">
-                    <button type="submit" class="bouton btn btn-warning"><p class="catForm">Envoyer</p></button>
+                    <button type="submit" class="btn btn-warning"><p class="catForm">Envoyer</p></button>
                 </div>
             </form>
         </div>
