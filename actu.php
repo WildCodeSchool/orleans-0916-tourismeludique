d4879fe3f7918775ae830e166935683f876195b2
Gy0m <landreaug@gmail.com>
2016-10-14 09:02:57 +0200

    caroussel fixed

--- actu.php
+++ actu.php
@@ -1,70 +1,132 @@
 <html>
-<div class="container-fluid actu" id="ancreActu">
-    <h1>Actualités</h1>
-    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-        <!-- Indicators -->
-        <ol class="carousel-indicators">
-            <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-            <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-            <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-        </ol>
+    <div class="container-fluid actu" id="ancreActu">
+        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+                <!-- Indicators -->
+            <ol class="carousel-indicators">
+                <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+            </ol>
 
-        <!-- Wrapper for slides -->
-        <div class="carousel-inner" role="listbox">
-            <div class="item active">
-                <div class="row">
-                    <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
-                        <h2>Actu 1</h2>
-                        <img src="image/P1030919rond.png" class="img-responsive imgInLens" alt="gargouille">
-                        <img src="image/lens_reflect.png"  class="img-responsive lens-actu" alt="lentille">
-                        <p class="txtActu">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet,
-                            consectetur adipisicing elit.<br/>
-                            <a href="#">En lire plus...</a>
-                        </p>
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner" role="listbox">
+                <div class="item active">
+                    <h1>Actualités</h1>
+                    <div class="row">
+                        <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                            <h2>Actu 1</h2>
+                            <img src="image/P1030919rond.png" class="img-responsive imgInLens" alt="gargouille">
+                            <img src="image/lens_reflect.png"  class="img-responsive lens-actu" alt="lentille">
+                            <p class="txtActu">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet,
+                                consectetur adipisicing elit.<br/>
+                                <a href="#">En lire plus...</a>
+                            </p>
+                        </div>
+                        <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                            <h2>Actu 2</h2>
+                            <img src="image/P1030919rond.png" class="img-responsive imgInLens" alt="gargouille">
+                            <img src="image/lens_reflect.png"  class="img-responsive lens-actu" alt="lentille">
+                            <p class="txtActu">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet,
+                                consectetur adipisicing elit.<br/>
+                                <a href="#">En lire plus...</a>
+                            </p>
+                        </div>
+                        <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                            <h2>Actu 3</h2>
+                            <img src="image/P1030919rond.png" class="img-responsive imgInLens" alt="gargouille">
+                            <img src="image/lens_reflect.png"  class="img-responsive lens-actu" alt="lentille">
+                            <p class="txtActu">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet,
+                                consectetur adipisicing elit.<br/>
+                                <a href="#">En lire plus...</a>
+                            </p>
+                        </div>
                     </div>
-                    <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
-                        <h2>Actu 2</h2>
-                        <img src="image/P1030919rond.png" class="img-responsive imgInLens" alt="gargouille">
-                        <img src="image/lens_reflect.png"  class="img-responsive lens-actu" alt="lentille">
-                        <p class="txtActu">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet,
-                            consectetur adipisicing elit.<br/>
-                            <a href="#">En lire plus...</a>
-                        </p>
-                    </div>
-                    <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
-                        <h2>Actu 3</h2>
-                        <img src="image/P1030919rond.png" class="img-responsive imgInLens" alt="gargouille">
-                        <img src="image/lens_reflect.png"  class="img-responsive lens-actu" alt="lentille">
-                        <p class="txtActu">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet,
-                            consectetur adipisicing elit.<br/>
-                            <a href="#">En lire plus...</a>
-                        </p>
-                    </div>
-                </div>
-                <div class="carousel-caption">
-                </div>
-                <div class="item">
-
                     <div class="carousel-caption">
-
+                    ...
                     </div>
                 </div>
                 <div class="item">
-
+                    <h1>Actualités</h1>
+                    <div class="row">
+                        <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                            <h2>Actu 1</h2>
+                            <img src="image/P1030919rond.png" class="img-responsive imgInLens" alt="gargouille">
+                            <img src="image/lens_reflect.png"  class="img-responsive lens-actu" alt="lentille">
+                            <p class="txtActu">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet,
+                                consectetur adipisicing elit.<br/>
+                                <a href="#">En lire plus...</a>
+                            </p>
+                        </div>
+                        <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                            <h2>Actu 2</h2>
+                            <img src="image/P1030919rond.png" class="img-responsive imgInLens" alt="gargouille">
+                            <img src="image/lens_reflect.png"  class="img-responsive lens-actu" alt="lentille">
+                            <p class="txtActu">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet,
+                                consectetur adipisicing elit.<br/>
+                                <a href="#">En lire plus...</a>
+                            </p>
+                        </div>
+                        <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                            <h2>Actu 3</h2>
+                            <img src="image/P1030919rond.png" class="img-responsive imgInLens" alt="gargouille">
+                            <img src="image/lens_reflect.png"  class="img-responsive lens-actu" alt="lentille">
+                            <p class="txtActu">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet,
+                                consectetur adipisicing elit.<br/>
+                                <a href="#">En lire plus...</a>
+                            </p>
+                        </div>
+                    </div>
                     <div class="carousel-caption">
-
+                        ...
                     </div>
                 </div>
-                <!-- Controls -->
-                <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-                    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-                    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                </a>
+                <div class="item">
+                    <h1>Actualités</h1>
+                    <div class="row">
+                        <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                            <h2>Actu 1</h2>
+                            <img src="image/P1030919rond.png" class="img-responsive imgInLens" alt="gargouille">
+                            <img src="image/lens_reflect.png"  class="img-responsive lens-actu" alt="lentille">
+                            <p class="txtActu">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet,
+                                consectetur adipisicing elit.<br/>
+                                <a href="#">En lire plus...</a>
+                            </p>
+                        </div>
+                        <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                            <h2>Actu 2</h2>
+                            <img src="image/P1030919rond.png" class="img-responsive imgInLens" alt="gargouille">
+                            <img src="image/lens_reflect.png"  class="img-responsive lens-actu" alt="lentille">
+                            <p class="txtActu">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet,
+                                consectetur adipisicing elit.<br/>
+                                <a href="#">En lire plus...</a>
+                            </p>
+                        </div>
+                        <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                            <h2>Actu 3</h2>
+                            <img src="image/P1030919rond.png" class="img-responsive imgInLens" alt="gargouille">
+                            <img src="image/lens_reflect.png"  class="img-responsive lens-actu" alt="lentille">
+                            <p class="txtActu">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet,
+                                consectetur adipisicing elit.<br/>
+                                <a href="#">En lire plus...</a>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="carousel-caption">
+                        ...
+                    </div>
+                </div>
+                 <!-- Insérer ici un nouveau slide -->
             </div>
+
+            <!-- Controls -->
+            <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
         </div>
     </div>
 </html>
