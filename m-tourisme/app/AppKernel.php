5c390a3f23ea8afbd7a9d7513ebb7a02a8e7d49a
MathieuM1 <mat.mercier45@gmail.com>
2016-10-21 13:57:13 +0200

    add module Log in

--- m-tourisme/app/AppKernel.php
+++ m-tourisme/app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new MTLBundle\MTLBundle(),
             new AdminBundle\AdminBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
---
+++ m-tourisme/src/AdminBundle/Entity/User.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: m21
+ * Date: 21/10/16
+ * Time: 11:51
+ */
+
+namespace AdminBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+
+
+}
