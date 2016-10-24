5486bb294a35578c7beb086ecf0419b6c905c29c
Gy0m <landreaug@gmail.com>
2016-10-24 16:58:14 +0200

    ajout photos par actu

--- m-tourisme/src/MTLBundle/Controller/DefaultController.php
+++ m-tourisme/src/MTLBundle/Controller/DefaultController.php
@@ -15,18 +15,15 @@ class DefaultController extends Controller
     /**
      * @Route("/index")
      */
-
     public function listeActuIndexAction()
     {
         $actus = $this->getDoctrine()
             ->getRepository('AdminBundle:actu')
             ->findAll();
-
         return $this->render('MTLBundle:Default:index.html.twig', array(
             'actus' => $actus,
         ));
     }
-
     /**
      * @Route("/listeactu", name="liste_actu")
      */
@@ -35,13 +32,10 @@ class DefaultController extends Controller
         $actus = $this->getDoctrine()
             ->getRepository('AdminBundle:actu')
             ->findAll();
-
         return $this->render('MTLBundle:Default:listeActu.html.twig', array(
             'actus' => $actus,
         ));
     }
-
-
     /**
      * Finds and displays an actu entity.
      *
@@ -53,36 +47,9 @@ class DefaultController extends Controller
 //        $oneactu = $this->getDoctrine()
 //            ->getRepository('AdminBundle:actu')
 //            ->findOneById($id);
-
         return $this->render('MTLBundle:Default:oneActu.html.twig', array(
             'actu' => $actu,
         ));
-    }
-
-    /**
-     * @Route("/index")
-     * @Method({"GET", "POST"})
-     */
-    public function ActuAction()
-    {
-        $actu = new actu();
-        $file = $actu->getImage();
-        $file->move(
-            $this->getParameter('upload_directory'),
-            $fileName
-        );
-        $actu->setImage($fileName);
-
-      //  $em = $this->getDoctrine()->getManager();
-       // $em->persist($actu);
-       // $em->flush();
-
-        return $this->redirectToRoute('listeactu', array('id' => $actu->getId()));
-
-        return $this->render('actu/new.html.twig', array(
-            'actu' => $actu
-        ));
-
     }
 
 }
